e5da8810ce064a7b13b5b4e7e577e90626aa9fd5
seanq101 <seanq101@gmail.com>
2018-01-08 17:54:17 -0800

    Added extra tests to pt 3

--- lab1/part3/part3_tests.c
+++ lab1/part3/part3_tests.c
@@ -9,11 +9,36 @@ void test_sum_1(){
 	if (res == 10){
 		printf("Test 1 passed\n");
 	}
+	else{
+		printf("Test 1 failed\n");
+	}
+}
+
+void test_sum_2(){
+	int res = sum(arr, 0);
+	if (res == 0){
+		printf("Test 2 passed\n");
+	}
+	else{
+		printf("Test 2 failed\n");
+	}
+}
+
+void test_sum_3(){
+	int res = sum(arr, 4);
+	if (res == 6){
+		printf("Test 3 passed\n");
+	}
+	else{
+		printf("Test 3 failed\n");
+	}
 }
 
 void test_sum()
 {
    test_sum_1();
+   test_sum_2();
+   test_sum_3();
    
 }
 
