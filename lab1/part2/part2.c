cc833cb28388c202a9f2dd953090b2b53c4188d3
seanq101 <seanq101@gmail.com>
2018-01-08 17:06:42 -0800

    Part 2 added initial commit

---
+++ lab1/part2/part2.c
@@ -0,0 +1,6 @@
+#include <math.h>
+#include <part2.h>
+
+double calc (double x){
+	return sqrt(x + 37) * 9 - 14;
+}
--- lab1/part2/part2_tests.c
+++ lab1/part2/part2_tests.c
@@ -8,9 +8,21 @@ void test_calc_1()
    checkit_double(calc(-1.0), 40);
 }
 
+void test_calc_2()
+{
+   checkit_double(calc(12), 49);
+}
+
+void test_calc_3()
+{
+   checkit_double(calc(-12), 31);
+}
+
 void test_calc()
 {
    test_calc_1();
+   test_calc_2();
+   test_calc_3()
 }
 
 int main(void)
