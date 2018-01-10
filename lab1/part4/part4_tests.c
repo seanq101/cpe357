578714a970b5d528db1acfb2e43f30ce8c42d33b
seanq101 <seanq101@gmail.com>
2018-01-10 13:19:52 -0800

    Added some more basic tests

--- lab1/part4/part4_tests.c
+++ lab1/part4/part4_tests.c
@@ -2,23 +2,59 @@
 
 #include "part4.h"
 
-char mystr [6] = "HeLlO";
+
 
 void test_strlower_1(){
+	char mystr [6] = "HeLlO";
 	char buff[6] = "     ";
 	int idx = 0;
 	int temp;
 	strlower(mystr, buff);
 	
 	while (buff[idx]){
-		printf("Hello\n");
 		temp = buff[idx];
 		if (temp <= 96 || temp >= 122){
 			printf("Test 1 failed\n");		
 		}
 		idx++;
 	}
-	printf("Test 1 passed\n");
+	printf("Test 1 passed, %s\n", buff);
+	
+}
+
+void test_strlower_2(){
+	char mystr [6] = "HeLlO";
+	char buff[6] = "     ";
+	int idx = 0;
+	int temp;
+	strlower(mystr, buff);
+	
+	while (buff[idx]){
+		temp = buff[idx];
+		if (temp <= 96 || temp >= 122){
+			printf("Test 2 failed\n");		
+		}
+		idx++;
+	}
+	printf("Test 2 passed, %s\n", buff);
+	
+}
+
+void test_strlower_3(){
+	char mystr [6] = "";
+	char buff[6] = "     ";
+	int idx = 0;
+	int temp;
+	strlower(mystr, buff);
+	
+	while (buff[idx]){
+		temp = buff[idx];
+		if (temp <= 96 || temp >= 122){
+			printf("Test 3 failed\n");		
+		}
+		idx++;
+	}
+	printf("Test 3 passed\n");
 	
 }
 
@@ -26,6 +62,8 @@ void test_strlower_1(){
 void test_strlower()
 {
    test_strlower_1();
+   test_strlower_2();
+   test_strlower_3();
    
    
 }
