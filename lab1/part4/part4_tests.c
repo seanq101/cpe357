0d49b2440c38312fbec398f7706c69ac0ae66d98
seanq101 <seanq101@gmail.com>
2018-01-10 13:44:01 -0800

    Added more tests part4

--- lab1/part4/part4_tests.c
+++ lab1/part4/part4_tests.c
@@ -75,13 +75,49 @@ void test_str_lowermutate_1(){
 	
 }
 
+void test_str_lowermutate_2(){
+	char mystr [6] = "hello";
+	int idx = 0;
+	int temp;
+	str_lowermutate(mystr);
+	
+	while (mystr[idx]){
+		temp = mystr[idx];
+		if (temp <= 96 || temp >= 122){
+			printf("Test 2 mutate failed\n");		
+		}
+		idx++;
+	}
+	printf("Test 2 mutate passed, %s\n", mystr);
+	
+}
+
+void test_str_lowermutate_3(){
+	char mystr [6] = "";
+	int idx = 0;
+	int temp;
+	str_lowermutate(mystr);
+	
+	while (mystr[idx]){
+		temp = mystr[idx];
+		if (temp <= 96 || temp >= 122){
+			printf("Test 3 mutate failed\n");		
+		}
+		idx++;
+	}
+	printf("Test 3 mutate passed, %s\n", mystr);
+	
+}
+
 void test_strlower()
 {
    test_strlower_1();
    test_strlower_2();
    test_strlower_3();
    test_str_lowermutate_1();
-   
+   test_str_lowermutate_2();
+   test_str_lowermutate_3();
+
 }
 
 int main(void)
