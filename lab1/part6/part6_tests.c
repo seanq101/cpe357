d860bca2bc915b66d96f057a3342679183c26d0a
seanq101 <seanq101@gmail.com>
2018-01-10 18:12:35 -0800

    Part 6 tests

--- lab1/part6/part6_tests.c
+++ lab1/part6/part6_tests.c
@@ -20,16 +20,39 @@ void test_create_point2(void)
    checkit_double(p.y, 12.1);
 }
 
+void test_is_a_square1(void){
+   int res;
+   struct point p1 = create_point(1,1);
+   struct point p2 = create_point(2,0);
+   struct rectangle r = create_rectangle(p1,p2);
+   res = is_a_square(r);
+   checkit_int(res,1);
+}
+
+void test_is_a_square1(void){
+   int res;
+   struct point p1 = create_point(0,0);
+   struct point p2 = create_point(2,1);
+   struct rectangle r = create_rectangle(p1,p2);
+   res = is_a_square(r);
+   checkit_int(res,0);
+}
+
 void test_create_point(void)
 {
    test_create_point1();
    test_create_point2();
 }
 
+void test_create_rectangle(void){
+   test_is_a_square1();
+   test_is_a_square2();
+}
+
 int main(int arg, char *argv[])
 {
    /* call testing function(s) here */
    test_create_point();
-
+   test_create_rectangle();
    return 0;
 }
