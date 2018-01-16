cceb1032e2704aa922d06bc9c030cae52cf469da
seanq101 <seanq101@gmail.com>
2018-01-16 12:21:10 -0800

    Trying to open a file

--- hw1/mytr.c
+++ hw1/mytr.c
@@ -1,3 +1,29 @@
 #include <stdin.h>
 #include <stdout.h>
 #include <stderr.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "mytr.h"
+
+int main(int argc, char *argv[]) {
+
+	FILE *file;
+
+
+	if(argc != 5) {
+		printf("mytr: improper number of arguments\nUsage: %s <filename> <word>\n", argv[0]);
+		return 1;
+	}
+
+
+	file = fopen(argv[1], "r");
+		
+	if(file == NULL) {
+		printf("Unable to open file: %s\n", argv[1]);
+		return 1;	
+	}
+
+	printf("%s %s %s %s %s\n", argv[0], argv[1], argv[2],argv[3],argv[4]);
+
+}
