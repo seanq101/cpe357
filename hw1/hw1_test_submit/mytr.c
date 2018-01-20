442eb2d418efdc9f6301f109378bdef129ef3c6b
seanq101 <seanq101@gmail.com>
2018-01-19 18:57:57 -0800

    trying to test for make

---
+++ hw1/hw1_test_submit/mytr.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "mytr.h"
+
+
+int main(int argc, char *argv[]) {
+
+	int result;
+	result = checkArgs(argc, argv);
+	if( result == 1 ){
+		return 1;
+	}
+
+	initArr();
+
+	if(result == -1){
+		buildDelArr(argv[2]);
+		translate(true);
+	}else{
+		buildArr(argv[1], argv[2]);
+		translate(false);
+	}
+
+	return 0;
+}
+
+
+
+int checkArgs(int argc, char *argv[]){
+	if( argc < 3){
+		printf("tr: missing operand\nTry 'tr --help' for more information.\n");
+		return 1;
+	}else if(strcmp(argv[1],"-d") == 0 && argc > 3){
+		printf("tr: extra operand '%s'\nOnly one string may be given when deleting without squeezing repeats.\nTry 'tr --help' for more information.\n", argv[3]);
+		return 1;
+	}else if (argc > 3){
+		printf("tr: extra operand ‘%s’\nTry 'tr --help' for more information.\n", argv[3]);
+		return 1;
+	}else if(strcmp(argv[1],"-d") == 0){
+		return -1;
+	}
+	return 0;
+}
+
+
+
+
+void buildDelArr(char *setA){
+	while(*setA){
+		if(*setA == '\\' && *(setA + 1)){
+			setA++;
+			*setA = findEscaped(*setA);
+		}
+
+		arr[(int) *setA] = '\0';
+		setA++;
+	}
+}
+
+int checkEscaped(char c){
+	switch (c)
+	{
+		case 't':
+			return 1;
+		case 'n':
+			return 1;
+		case '\\':
+			return 1;
+		default:
+			return 0;
+	}
+}
+
+char findEscaped(char c){
+	
+	switch (c)
+	{
+		case 't':
+			return '\t';
+		case 'n':
+			return '\n';
+		case '\\':
+			return '\\';
+		default:
+			return c;
+	}
+}
+
+void initArr(){
+	int index;
+	for(index = 0; index < 256; index++){
+		arr[index] = index;
+	}
+}
+
+void buildArr(char *setA, char *setB){
+
+	while(*setA){
+		if(*setA == '\\' && *(setA + 1) && (checkEscaped(*(setA + 1)) == 0) ) {
+			setA++;
+			arr[(int) *setA] = *setA;
+			setA++;
+		}else{
+			if(*setB == '\\' && *(setB + 1) && (checkEscaped(*(setB + 1)) == 1) ){
+				setB++;
+				*setB = findEscaped(*setB);
+			}
+
+			if(*setA == '\\' && *(setA + 1) && (checkEscaped(*(setA + 1)) == 1) ){
+				setA++;
+				*setA = findEscaped(*setA);
+			}
+		
+
+			arr[(int) *setA] = *setB;
+			setA++;
+		}
+
+		if(*(setB + 1)){
+			setB++;
+		}
+	}
+}
+
+
+void translate(bool del){
+	char c;
+
+	while((c = getchar()) != EOF){
+		if(del == false){
+			putchar(arr[(int) c]);
+		}else if(arr[(int) c] == c){
+			putchar(c);
+		}
+	}
+}
+
---
+++ hw1/hw1_test_submit/mytr.h
@@ -0,0 +1,19 @@
+#ifndef MYTR_H
+#define MYTR_H
+
+typedef int bool;
+#define true 1
+#define false 0
+
+char arr[256];
+
+char findEscaped(char c);
+void initArr();
+void buildArr(char *setA, char *setB);
+void translate(bool del);
+void buildDelArr(char *setA);
+int checkArgs(int argc, char *argv[]);
+char findEscaped(char c);
+int checkEscaped(char c);
+
+#endif
