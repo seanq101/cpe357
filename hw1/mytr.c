5ea3f657ed03d5c0e055b398ebcced694a1bbfeb
seanq101 <seanq101@gmail.com>
2018-01-21 22:53:33 -0800

    edited comp

--- hw1/mytr.c
+++ hw1/mytr.c
@@ -35,8 +35,8 @@ int checkArgs(int argc, char *argv[]){
 	}else if( argc < 3 && strcmp(argv[1],"-d") == 0){
 		printf("tr: missing operand\nTry 'tr --help' for more information.\n");
 		return 1;
-	}else if( argc < 3 && strcmp(argv[1][0], '-') == 0 && strcmp(argv[1],"-d") != 0){
-		printf("tr: invalid option -- '%s'\nTry 'tr --help' for more information.\n", argv[1][1]);
+	}else if( argc < 3 && argv[1][0] == '-' && strcmp(argv[1],"-d") != 0){
+		printf("tr: invalid option -- '%c'\nTry 'tr --help' for more information.\n", argv[1][1]);
 		return 1;
 	}else if(strcmp(argv[1],"-d") == 0 && argc > 3){
 		printf("tr: extra operand '%s'\nOnly one string may be given when deleting without squeezing repeats.\nTry 'tr --help' for more information.\n", argv[3]);
