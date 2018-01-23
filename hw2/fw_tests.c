59faa3fe41bc7a4ea13b69e945be506eeed32b32
seanq101 <seanq101@gmail.com>
2018-01-22 18:07:51 -0800

    added another one

--- hw2/fw_tests.c
+++ hw2/fw_tests.c
@@ -6,6 +6,7 @@
 void test_isNumber(){
 	char *b;
 	char *a;
+	char *fishsticks;
 	a = "-123";
 	b = "123";
 	if(isNumber(a) == 1){
@@ -17,6 +18,11 @@ void test_isNumber(){
 		printf("Test passed\n");
 	}else{
 		printf("123 Failed\n");
+	}
+	if(isNumber(c) == 1){
+		printf("Test passed\n");
+	}else{
+		printf("fishsticks Failed\n");
 	}
 }
 
