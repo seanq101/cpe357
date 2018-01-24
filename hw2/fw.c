54991ca2629979659619f65b06d6235b572fe0d9
seanq101 <seanq101@gmail.com>
2018-01-24 13:43:46 -0800

    NAH FAM MANS NOT HOT

--- hw2/fw.c
+++ hw2/fw.c
@@ -184,10 +184,11 @@ int ht_hash(char *key ) {
 }
 
 void printTable(struct map_table *t){
-	printf("Hi2\n");
 	int index;
 	for (index = 0; index < t->map_size; index++){
-		printf("Element:\t%d\tValue:\t%s\tFrequency:%d", index, t->list[index]->value, t->list[index]->frequency);
+		if(t->list[index]){
+			printf("Element:\t%d\tValue:\t%s\tFrequency:%d", index, t->list[index]->value, t->list[index]->frequency);
+		}
 	}
 }
 
