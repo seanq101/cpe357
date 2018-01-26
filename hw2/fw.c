0c7bb93d2f324b473ce4b3f7ec8350ab6a4d8851
seanq101 <seanq101@gmail.com>
2018-01-25 21:00:26 -0800

    y tho

--- hw2/fw.c
+++ hw2/fw.c
@@ -250,7 +250,7 @@ void printTable(struct map_table *t){
 
 	printf("Hi 4\n");
 
-	for (index = 0; index < t->used_size; index++){
+	for (index = 0; index < 5; index++){
 		
 			printf("Element:\t%d\tValue:\t%s\tFrequency:%d\n", index, vector[index].value, vector[index].frequency);
 		
