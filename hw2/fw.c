820a6713c113944b412aee83ea9609d9774ce30c
seanq101 <seanq101@gmail.com>
2018-01-26 13:48:25 -0800

    whozika

--- hw2/fw.c
+++ hw2/fw.c
@@ -154,7 +154,7 @@ struct map_table * addToTable(struct map_table *t, char *currentWord){
 
 	if( ((double) t->used_size / t->map_size ) >= .90) {
 		/* newMap = createBlankTable(t->map_size * 2); */
-		t->list = (struct map_element **)realloc(t->list, sizeof(struct map_element) * t->map_size * 2);
+		t->list = (struct map_element **)realloc(t->list, sizeof(struct map_element*) * t->map_size * 2);
 		t = reassignNewMap(newMap, t);
 	}
 	return t;
