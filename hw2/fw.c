7822b5fffa5b6bc4e215a8f3ffc7edf9e9d9fea7
seanq101 <seanq101@gmail.com>
2018-01-26 11:23:26 -0800

    teehee

--- hw2/fw.c
+++ hw2/fw.c
@@ -115,7 +115,7 @@ struct map_table * parseFile(FILE *f, struct map_table *t){
 	currentWord[index] = '\0';
 	*/
 	char *currentWord;
-	while(currentWord = readWord(f, 40) != ".'")
+	while(currentWord = readWord(f, 40) != ".")
 		t = addToTable(t, currentWord);
 	return t;
 
