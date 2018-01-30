cfc80985a093b24d6006c7219687e1be942815c2
seanq101 <seanq101@gmail.com>
2018-01-29 17:13:16 -0800

    checking my table

--- hw2/fw.c
+++ hw2/fw.c
@@ -316,7 +316,7 @@ void printTable(struct map_table *t){
 	
 	sortMap(t);
 	printf("The top %i words (out of %i) are:\n", freqOutput, t->used_size);
-	for (index = 0; index < freqOutput; index++){
+	for (index = 0; index < 200; index++){
 			printf("\t%i\t%s\n", t->list[index]->frequency,t->list[index]->value);
 	}
 
