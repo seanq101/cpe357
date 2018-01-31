7baff08096584ad6fdfcb44fc8a9daea60999295
seanq101 <seanq101@gmail.com>
2018-01-31 11:50:39 -0800

    put in lab2 V2

---
+++ lab2.2/readline.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#define CHUNK 80
+extern char *readline(FILE *infile) {
+/* Read a string from given stream. Returns the string, or NULL if
+* EOF is encoutered. Approach: allocate a buffer of size CHUNK, 10
+* and as the string grows expand the buffer as necessary
+*/
+int i;
+char *buff;
+char *ret;
+int size=0;
+char c;
+size=CHUNK;
+if(NULL==(buff=(char*)malloc(size * sizeof(char)))) { 20
+perror( FUNCTION );
+exit(−1);
+}
+for(i=0,c=getc(infile); c!=EOF ;c=getc(infile)) {
+if(i>=size−1) { /* buffer is too small, expand it. */
+size+=CHUNK;
+if(NULL==(buff=(char*)realloc(buff,size * sizeof(char)))) {
+perror( FUNCTION );
+exit(−1);
+} 30
+}
+buff[i++]=c;
+if ( c == ’\n’ )
+break;
+}
+if ( i ) { /* if there was a string read, copy it
+* into a new buffer. Otherwise, return
+* NULL to signal EOF
+*/ 40
+buff[i]=’\0’; /* final nul */
+if(NULL==(ret=(char*)malloc(i+1))) {
+perror( FUNCTION );
+exit(−1);
+}
+strcpy(ret,buff);
+} else {
+ret = NULL;
+}
+50
+free(buff);
+return ret;
+}
---
+++ lab2.2/uniq.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "readline.h"
+int main(int argc, char *argv[]){
+/* read lines from stdin until there are no more lines. For each line,
+* compare it to the previous line. If they are the different, print
+* the previous line. If the same, discard the previous line.
+*/ 10
+char *last, *next;
+last = readline(stdin); /* read an initial line */
+/* now, keep reading lines until there are no more lines */
+while ( (NULL != last) && (NULL != (next=readline(stdin)))) {
+if ( strcmp(last, next) ) { /* print the old line if different */
+fputs(last, stdout);
+}
+free(last); /* we’re done with last now */ 20
+last = next;
+}
+if ( last ) /* print the last line if there is one */
+fputs(last, stdout);
+return 0;
+}
