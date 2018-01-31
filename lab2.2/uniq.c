293348aff5939b12ab25fbac955476508a56d5b8
seanq101 <seanq101@gmail.com>
2018-01-31 13:28:03 -0800

    went back to his version

--- lab2.2/uniq.c
+++ lab2.2/uniq.c
@@ -8,20 +8,16 @@ int main(int argc, char *argv[]){
 * the previous line. If the same, discard the previous line.
 */ 
 char *last, *next;
-FILE *f;
-int index = 1;
-f = fopen(argv[1], "r");
-last = readline(f); /* read an initial line */
+last = readline(stdin); /* read an initial line */
 /* now, keep reading lines until there are no more lines */
-for(index = 1; index < argc; index++){
-	while ( (NULL != last) && (NULL != (next=readline(f) ) ) ) {
+	while ( (NULL != last) && (NULL != (next=readline(stdin) ) ) ) {
 		if ( strcmp(last, next) ) { /* print the old line if different */
 			fputs(last, stdout);
 		}
 		free(last); /* we’re done with last now */ 
 		last = next;
 	}
-}
+
 if ( last ) /* print the last line if there is one */
 	fputs(last, stdout);
 return 0;
