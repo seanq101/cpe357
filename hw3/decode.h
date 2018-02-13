f454a7a2aae9d777999fa4faeb44e6d19696efd1
seanq101 <seanq101@gmail.com>
2018-02-12 17:27:28 -0800

    fuck the share stuff

--- hw3/decode.h
+++ hw3/decode.h
@@ -1,6 +1,30 @@
 #ifndef DECODEH
 #define DECODEH
 
+#define SIZE 256
+
+struct node {
+	int frequency;
+	char value;
+	struct node *left;
+	struct node *right;
+	int justAdded;
+};
+
+int freqArr [SIZE];
+
+int codeArr[SIZE][20];
+
+
+
+void initCodeArr();
+struct node ** create_node_list();
+int comparator(const void *p, const void *q);
+struct node * form_tree(struct node ** list);
+struct node * take_two_lowest(struct node **list);
+void freeEverything(struct node* list);
+
+
 void readHeader(int fdin);
 void readBody(int fdin,int fdout,struct node* currentNode,struct node * root);
 void printFreqArr();
--- hw3/encode.h
+++ hw3/encode.h
@@ -15,6 +15,14 @@ int freqArr [SIZE];
 
 int codeArr[SIZE][20];
 
+void initCodeArr();
+struct node ** create_node_list();
+int comparator(const void *p, const void *q);
+struct node * form_tree(struct node ** list);
+struct node * take_two_lowest(struct node **list);
+void freeEverything(struct node* list);
+
+
 void makeUnixTable(int fdin);
 void unixWriteToFile(int fdout, char* argv1);
 void write_code(char letter, int fdout);
