7cf3cde4a21df9d12a245476f6639a040223cdf5
seanq101 <seanq101@gmail.com>
2018-02-18 16:46:09 -0800

    signings

--- lab5/mypwd.c
+++ lab5/mypwd.c
@@ -31,7 +31,7 @@ void listDir(char *path){
 	}
 	ent1 = readdir(d);
 	ent2 = readdir(d);
-	if(ent1->ino_t != ent2->ino_t){
+	if(ent1->d_ino != ent2->d_ino){
 		strcpy(buf, ent1->d_name);
 		buf[PATH_MAX + 1] = '\0';
 		strncat(buf, path, PATH_MAX);
